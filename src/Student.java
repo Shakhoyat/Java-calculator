5422dcf8899f5dfdc08b39b91bf5b7eefee90176
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-20 22:34:43 +0600

    added student class

---
+++ src/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    private String name;
+    private int id;
+
+    // constructor
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("slkgfnsdjk");
+    }
+}
