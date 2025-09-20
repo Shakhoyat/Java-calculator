fcf2fad0cadb7f26ca9dce4af1b4d780f3eefc30
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-20 22:44:33 +0600

    implemented simple calculator function

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,20 @@
+public class SimpleCalculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Can't divide by zero");
+        }
+        return (double) a / b;
+    }
+}
